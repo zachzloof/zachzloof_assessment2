b3b6126714c97f37238e1d8fbc5949b764acb809
Zach Zloof <zachzloof@hotmail.com>
2022-04-14 09:26:57 +0100

    CN-27 #comment ZZ: Basic read methods implemented to the ChampController.java class

--- cloud-native-project/src/main/java/com/qa/demo/web/ChampController.java
+++ cloud-native-project/src/main/java/com/qa/demo/web/ChampController.java
@@ -1,8 +1,12 @@
 package com.qa.demo.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +30,16 @@ public class ChampController {
 		Champ create = this.service.create(c);
 		ResponseEntity<Champ> res = new ResponseEntity<Champ>(create, HttpStatus.CREATED);
 		return res;
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Champ>> getAllChampions() {
+		return ResponseEntity.ok(this.service.getAll());
+	}
+	
+	@GetMapping("/get/{id}")
+	public Champ getChampionById(@PathVariable Integer id) {
+		return this.service.getById(id);
 	}
 
 }
