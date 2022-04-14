5941193ff8bc81bb5da4a618c5fb29bf479360a4
Zach Zloof <zachzloof@hotmail.com>
2022-04-14 09:49:46 +0100

    CN-27 #comment ZZ: The delete method has been implemented to the ChampController.java class, the feature is now ready to merge

--- cloud-native-project/src/main/java/com/qa/demo/web/ChampController.java
+++ cloud-native-project/src/main/java/com/qa/demo/web/ChampController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,6 +73,12 @@ public class ChampController {
 		Champ body = this.service.replace(id, newChampion);
 		ResponseEntity<Champ> res = new ResponseEntity<Champ>(body, HttpStatus.ACCEPTED);
 		return res;
+	}
+	
+	@DeleteMapping("/remove/{id")
+	public ResponseEntity<?> deleteChampion(@PathVariable Integer id) {
+		this.service.remove(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 }
